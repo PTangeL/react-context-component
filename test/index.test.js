1c4eb9a828726b703ddf8f46aed02bba8b90c9dc
alexlbr <alex.lobera@gmail.com>
2017-08-19 13:41:14 +0100

    added withContext HOC

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import renderer from 'react-test-renderer'
-import Context from '../src'
+import { Context } from '../src'
+import { withContext } from '../src'
 
-const RenderDataContext = (props, { data }) => <p>{data}</p>
+const RenderDataContext = (props, { data }) => (
+  <p>data from context: {data}, data from props {props.data}</p>
+)
 
 RenderDataContext.contextTypes = {
   data: PropTypes.number.isRequired
 }
 
-it('renders correctly', () => {
+it('renders Context correctly', () => {
   const tree = renderer
     .create(
       <Context data={1}>
         <RenderDataContext />
+      </Context>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders a component withContext correctly', () => {
+  const RenderWithDataContext = withContext({
+    data: PropTypes.number.isRequired
+  })(RenderDataContext)
+
+  const tree = renderer
+    .create(
+      <Context data={1}>
+        <RenderWithDataContext />
       </Context>
     )
     .toJSON()
